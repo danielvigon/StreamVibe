7d6c0732e5d066b3ac569836638c06c933b2467a
Daniel Vilela <danielvg1136@gmail.com>
2025-03-17 16:40:44 -0300

    Adiciona o padrão de design MTV
    Adiciona a rota sistema

--- sistema/views.py
+++ sistema/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here (MTV - model-template-view).
+
+def index(request):
+    return render(
+        request,
+        'sistema/index.html',
+    )
+
+
+# request
+# response
