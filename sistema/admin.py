1c7a3786a9a750122de1f38ee6aaa16fa933335d
Daniel Vilela <danielvg1136@gmail.com>
2025-03-28 16:40:52 -0300

    Add: include some sistema  models

--- sistema/admin.py
+++ sistema/admin.py
@@ -7,3 +7,11 @@ from sistema import models
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'last_name', 'email', 'active',)
+
+@admin.register(models.Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'release_year', 'studio', 'genre',)
+
+@admin.register(models.Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
--- sistema/models.py
+++ sistema/models.py
@@ -3,15 +3,28 @@ from django.utils import timezone
 
 # Create your models here.
 
-# * User class model here. *
 class User (models.Model):
     name = models.CharField(max_length = 50)
     last_name = models.CharField(max_length = 50)
     cpf = models.CharField(max_length = 11)
-    phone_number = models.CharField(max_length = 20)
+    phone_number = models.CharField(max_length = 12)
     email = models.EmailField()
     address = models.CharField(max_length = 100)
     register_date = models.DateTimeField(default = timezone.now)
     active = models.BooleanField(default = True)
 
-    # photo =
+    def __str__(self):
+        return f"{self.name} {self.last_name}"
+
+class Movie(models.Model):
+    name = models.CharField(max_length=50)
+    release_year = models.IntegerField()
+    studio = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
+    synopsis = models.TextField(max_length=500)
+    register_date = models.DateField(default = timezone.now)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+    register_date = models.DateField(default = timezone.now)
+
